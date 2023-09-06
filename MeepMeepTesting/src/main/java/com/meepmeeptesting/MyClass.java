75474a2366e213330a5c17489fbe1b0459613de8
newwares <newwares123@gmail.com>
2023-09-06 15:56:47 -0400

    rr and meep

---
+++ MeepMeepTesting/src/main/java/com/meepmeeptesting/MyClass.java
@@ -0,0 +1,27 @@
+package com.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequence;
+import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequenceBuilder;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.setProperty("sun.java2d.uiScale", "1.0");
+        MeepMeep meepMeep = new MeepMeep(800);
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                .setStartPose(new Pose2d(36, 60))
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(300, 300, Math.toRadians(1440), Math.toRadians(720), 15).build();
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .setDarkMode(true)
+                .addEntity(myBot)
+                .start();
+    }
+}
